d41b9b5bff4211c5137a90b6af9a0d1b5853862c
Ahmed <AhmedITfixer@gmail.com>
2024-01-13 13:55:56 +0000

    I added the python concatenation

---
+++ data_types.py
@@ -0,0 +1,18 @@
+
+# literal assignment
+first = "Ahmed"
+last = "Mohamed"
+
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+# constructor function
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+
+# concatenation
+fullname = first + " " + last
+print(fullname)
